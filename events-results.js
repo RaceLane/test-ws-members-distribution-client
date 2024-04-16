d207b12c3b8b47966298b85c714902c0411b886a
Rodewitsch <rodewitsch@inbox.ru>
2024-04-16 15:41:05 +0300

    feat: remove auth block in events-results script

--- events-results.js
+++ events-results.js
@@ -1,23 +1,10 @@
 import { io } from "socket.io-client";
-import fetch from "node-fetch";
 
 (async () => {
   try {
     const event_id = process.env.event_id; //99
     const host = process.env.host; //'http://localhost:3000', 'https://api.racedevspace.com', 'https://api.racelane.io'
-    const login = process.env.login; //'test'
-    const password = process.env.password; //'test'
     const type = +process.env.type; //1
-
-    const response = await fetch(`${host}/api/v2/auth/signin`, {
-      method: 'post',
-      body: JSON.stringify({
-        login,
-        password
-      }),
-      headers: { 'Content-Type': 'application/json' }
-    });
-    const data = await response.json();
 
     const socket = io(`${host}/socket.io/v2/events/${event_id}/results`,
       {
@@ -28,7 +15,6 @@ import fetch from "node-fetch";
         extraHeaders: {
           'accept-language': 'ru',
         },
-        auth: { token: data.token }
       });
 
     socket.on("connect", () => {
